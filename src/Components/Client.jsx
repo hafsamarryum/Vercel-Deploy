60e4cac2794d2e9febef66e88b17b8ea13e409f2
unknown <hafsa.marryum@gmail.com>
2024-12-14 23:30:29 +0500

    review imges and cart removed

--- src/Components/Client.jsx
+++ src/Components/Client.jsx
@@ -6,10 +6,6 @@ import Himg2 from '../assest/client/Himg2.png';
 import Himg3 from '../assest/client/Himg3.png';
 import Himg4 from '../assest/client/Himg4.png';
 import Himg5 from '../assest/client/Himg5.png';
-import Himg6 from '../assest/client/Himg6.png';
-import Himg7 from '../assest/client/Himg7.png';
-import Himg8 from '../assest/client/Himg8.png';
-import Himg9 from '../assest/client/Himg9.png';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
@@ -54,35 +50,7 @@ const Client = () => {
       para: 'From start to finish, Sired Bits showcased their technical brilliance and creative. They made complex problems seem effortless and gave us a solution we’re proud to present to our customers.',
       img: Himg5,
       des: 'Business Manager',
-    },
-    {
-      id: 6,
-      title: 'Charles Max',
-      para: 'The team at Sired Bits was attentive to our specific requirements and delivered a product that fit perfectly with our business processes. The level of customization they offer is second to none.',
-      img: Himg6,
-      des: 'UX/UI Designer',
-    },
-    {
-      id: 7,
-      title: 'Isabelle Joseph',
-      para: 'Sired Bits provides top tier solutions without exceeding our budget. Their balance of affordability and quality is something you rarely see in the industry. Highly recommended!.',
-      img: Himg7,
-      des: 'Operations Manager',
-    },
-    {
-      id: 8,
-      title: 'Kevin Fox',
-      para: 'What stood out most was their ability to meet deadlines without compromising quality. Sired Bits is a team you can rely on, even for the most challenging projects.',
-      img: Himg8,
-      des: 'Project Manager',
-    },
-    {
-      id: 9,
-      title: 'Alex Adam',
-      para: 'What sets Sired Bits apart is their unwavering commitment to delivering exceptional results on time. Their team is not just skilled but also dependable.',
-      img: Himg9,
-      des: 'Senior Team Lead',
-    },
+    }
   ];
   const settings = {
     dots: true,                  
