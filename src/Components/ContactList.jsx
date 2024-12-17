5f826047d661ed33ca4e073e42abc176075d7e14
unknown <hafsa.marryum@gmail.com>
2024-12-18 02:37:58 +0500

    UpcontactLink

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,44 +1,54 @@
-import React from 'react'
-import location from '../assest/contactUs/location.png'
-import message from '../assest/contactUs/message.png'
-import phone from '../assest/contactUs/phone.png'
-import linkedin from '../assest/contactUs/linkedin.png'
+import React from 'react';
+import location from '../assest/contactUs/location.png';
+import message from '../assest/contactUs/message.png';
+import phone from '../assest/contactUs/phone.png';
+import linkedin from '../assest/contactUs/linkedin.png';
 
 const ContactList = () => {
-    const ContactListData = [
-        {
-              id: 1,
-              img: location ,
-              des: "G-108, DHA Phase 1, Lahore"
-        },
-        {
-              id: 2,
-              img: phone ,
-              des: "+92 341 3700 645"
-        },
-        {
-              id: 3,
-              img: message ,
-              des: "contact@siredbits.com"
-        },
-        {
-              id: 4,
-              img: linkedin ,
-              des: "https://SiredBits.com"
-        }
-    ]
-  return (
-    <>
-     {ContactListData.map((ele, i)=>{
-       return(
-        <div className="icon-li-wrapper" key={i}>
-        <img src={ele.img} alt="" srcset="" />
-        <p>{ele.des}</p>
-       </div>
-       )
-     })}
-    </>
-  )
-}
+      const ContactListData = [
+            {
+                  id: 1,
+                  img: location,
+                  des: "G-108, DHA Phase 1, Lahore",
+            },
+            {
+                  id: 2,
+                  img: phone,
+                  des: "+92 341 3700 645",
+            },
+            {
+                  id: 3,
+                  img: message,
+                  des: "contact@siredbits.com",
+            },
+            {
+                  id: 4,
+                  img: linkedin,
+                  des: "https://www.linkedin.com/company/siredbits",
+                  linkText: "linkedin/siredbits.com",
+                  isLink: true,
+            },
+      ];
 
-export default ContactList
+      return (
+            <>
+                  {ContactListData.map((ele, i) => (
+                        <div className="icon-li-wrapper" key={i}>
+                              <img src={ele.img} alt="icon" />
+                              {ele.isLink ? (
+                                    <a
+                                          href={ele.des}
+                                          target="_blank"
+                                          rel="noopener noreferrer"
+                                    >{ele.linkText} 
+                                    </a>
+                              ) : (
+                                    <p>{ele.des}</p>
+                              )}
+                        </div>
+                  ))}
+            </>
+      );
+};
+
+export default ContactList;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../Css/home.css'
-import girl from '../../src/assest/home/girl.png'
-import square from '../../src/assest/home/square.png'
-import triangle from '../../src/assest/home/triangle.png'
+// import girl from '../../src/assest/home/girl.png'
+// import square from '../../src/assest/home/square.png'
+// import triangle from '../../src/assest/home/triangle.png'
+import right from '../../src/assest/home/main-banner.webp'
 import { Link } from 'react-router-dom'
 
 
@@ -15,21 +16,22 @@ const Home = () => {
       <div className="home-outer">
         <div className="home-left">
           <h1 className='home-h1'>
-            Sired<span className="son">Bits</span> Tech Innovation
+            <span className='son'>Sired</span><span className="sired-p-home">Bits</span> <span className='AI-Home'>Software, AI Solutions and Consultancy</span>
           </h1>
           <p className="suffered-para">
             At SiredBits, we specialize in delivering Full-Stack Development services for Mobile, Web Applications, UI/UX, AI, DevOps, and SaaS frameworks  creating seamless, scalable, and high-performance solutions. <span className="suffered">Our team of expert </span>developers excels in building responsive, user-friendly applications that cater to diverse business needs.
           </p>
           <Link to='/ContactUs' className="custom-link" >
-          <div className="home-btn">Contact Us</div>
+            <div className="home-btn">Contact Us</div>
           </Link>
         </div>
         <div className="home-right">
-          <div className="home-girl-wrapper">
+          {/* <div className="home-girl-wrapper">
             <img src={girl} alt="not found" />
           </div>
           <img className="home-square" src={square} alt="not found" />
-          <img className="home-triangle" src={triangle} alt="not found" />
+          <img className="home-triangle" src={triangle} alt="not found" /> */}
+          <img src={right} alt="" srcset="" />
         </div>
       </div>
     </div>
